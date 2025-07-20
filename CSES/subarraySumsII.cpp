f4ed53e54638d082a10bcaf7e93c2ff50d9f74d9
luleoa12 <luleoa12@gmail.com>
2025-07-19 18:29:05 -0700

    July 18, 2025

---
+++ CSES/subarraySumsII.cpp
@@ -0,0 +1,20 @@
+// July 18, 2025
+// problem
+
+#include <bits/stdc++.h>
+using ll = long long;
+
+using namespace std;
+
+
+int main() {    
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n,x;
+    cin>>n>>x;
+    
+
+    return 0;
+}
+
--- USACO/Silver/hoofPaperScissors.cpp
+++ USACO/Silver/hoofPaperScissors.cpp
@@ -12,6 +12,38 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    freopen("hps.in", "r", stdin);
+    freopen("hps.out", "w", stdout);
+
+    int n;
+    cin>>n;
+
+    // what they choose
+    vector<int> hoof(n+1);
+    vector<int> scissors(n+1);
+    vector<int> paper(n+1);
+    for(int i=0;i<n;i++){
+        char x;
+        cin>>x;
+        scissors[i+1] = scissors[i];
+        paper[i+1] = paper[i];
+        hoof[i+1] = hoof[i];
+
+        if (x=='P') {
+            scissors[i+1] = scissors[i]+1;
+        } else if (x=='H') {
+            paper[i+1] = paper[i]+1;
+        } else{
+            hoof[i+1] = hoof[i]+1;
+        }
+    }
+    int ans=0;
+    for (int i=1;i<=n;i++){
+        int before = max(hoof[i], max(paper[i],scissors[i]));
+        int after = max({hoof[n]-hoof[i],paper[n]-paper[i],scissors[n]-scissors[i]});
+        ans=max(ans,after+before);
+    }
+    cout<<ans<<endl;
     return 0;
 }
 
